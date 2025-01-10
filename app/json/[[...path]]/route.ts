4238fe825b50e01ad661646f36d145f36412f6f4
yuya <kinjo.bobchin@gmail.com>
2025-01-10 11:01:46 +0900

    fix: Correct object property handling in POST and PUT
    - Added `omit` to exclude "id" from `reqBody` in PUT request.
    - Changed order of spreading properties for newItem in POST.

--- app/json/[[...path]]/route.ts
+++ app/json/[[...path]]/route.ts
@@ -3,7 +3,7 @@ import { getJsonData, toJsonString } from "@/app/json/[[...path]]/internal";
 import { type NextRequest, NextResponse } from "next/server";
 import { randomUUID } from "node:crypto";
 import { writeFileSync } from "node:fs";
-import { assocPath, findIndex, path, propEq, update } from "ramda";
+import { assocPath, findIndex, omit, path, propEq, update } from "ramda";
 
 // GET /json/[path]
 // GET /json/[path]/[id]
@@ -22,7 +22,7 @@ export async function GET(req: NextRequest) {
 export async function POST(req: NextRequest) {
   const { relativePaths, dynamicData, dbJson, reqBody } =
     await getJsonData(req);
-  const newItem = { id: randomUUID(), ...reqBody };
+  const newItem = { ...reqBody, id: randomUUID() };
   const updateData = [...[dynamicData].flat(), newItem];
   const updatedJson = assocPath(relativePaths, updateData, dbJson);
   writeFileSync(DB_JSON_PATH, toJsonString(updatedJson));
@@ -51,7 +51,7 @@ export async function PUT(req: NextRequest) {
   if (index === -1) {
     return NextResponse.json({ message: "Not found" }, { status: 404 });
   }
-  const updatedData = { ...detailData, ...reqBody };
+  const updatedData = { ...detailData, ...omit(["id"], reqBody) };
   const updatedJson = assocPath(
     middlePaths,
     update(index, updatedData, data),
